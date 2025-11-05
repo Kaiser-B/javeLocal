fb9ec21b03f0cf7c82fdbb62b3f306ada663329e
Víctor Sánchez <victorssanchez@hotmail.es>
2025-11-05 01:31:45 +0100

    parte entregable del proyecto paginas producto y categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page';
+import { ShopComponent } from './pages/shop/shop';
+import { ProductComponent } from './pages/product/product';
 
 export const routes: Routes = [
   {
     path:'',
     component:HomePageComponent,
   },
+
+  {
+    path: 'shop',
+    component: ShopComponent
+  },
+
+  { path: 'product',
+    component: ProductComponent
+  },
+
   {
     path:'**',
     redirectTo:''
-  }
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent } from './layout/navbar/navbar';
 import { HeadComponent } from './layout/head/head';
+import { FooterComponent } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HeadComponent],
+  imports: [RouterOutlet,NavbarComponent,HeadComponent,FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
